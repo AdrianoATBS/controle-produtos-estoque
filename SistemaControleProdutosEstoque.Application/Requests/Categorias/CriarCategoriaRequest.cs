bb193f24d3b14c446ceb9332b48371f1656f25a0
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-06-16 15:04:18 -0300

    refactor: ajusta acessibilidade da classe CriarCategoriaRequest

--- SistemaControleProdutosEstoque.Application/Requests/Categorias/CriarCategoriaRequest.cs
+++ SistemaControleProdutosEstoque.Application/Requests/Categorias/CriarCategoriaRequest.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace SistemaControleProdutosEstoque.Application.Requests.Categorias;
 
-namespace SistemaControleProdutosEstoque.Application.Requests.Categorias
+public class CriarCategoriaRequest
 {
-    internal class CriarCategoriaRequest
-    {
-    }
 }
