e8e182a98a7c09db786a17f967de124b9565545f
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-06-16 16:04:47 -0300

    feat(categoria): adiciona entidade Categoria com validações de domínio

--- SistemaControleProdutosEstoque.Domain/Entities/Categoria.cs
+++ SistemaControleProdutosEstoque.Domain/Entities/Categoria.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace SistemaControleProdutosEstoque.Domain.Entities;
 
-namespace SistemaControleProdutosEstoque.Domain.Entities
+public class Categoria
 {
-    internal class Categoria
+    public Guid Id { get; private set; }
+    public string Nome { get; private set; } = string.Empty;
+    public bool Ativo { get; private set; }
+    public DateTime DataCriacao { get; private set; }
+    private Categoria() { }
+    private Categoria(string nome)
     {
+        Id = Guid.NewGuid();
+        Nome = nome;
+        Ativo = true;
+        DataCriacao = DateTime.UtcNow;
     }
+    public static Categoria Criar(string nome)
+    {
+        ValidarNome(nome);
+        return new Categoria(nome);
+    }
+
+    private static void ValidarNome(string nome)
+    {
+        if (string.IsNullOrWhiteSpace(nome))
+            throw new ArgumentException("O nome da categoria é obrigatório.");
+    }
+    
+    public void CategoriaDesativada()
+    {
+        if(!Ativo)
+            throw new InvalidOperationException("A categoria já está desativada.");
+    Ativo = false;
+    }
+    
+    public void CategoriaAtivada()
+    {
+        if(Ativo)
+            throw new InvalidOperationException("A categoria já está ativa.");
+        Ativo = true;
+    }
+   
+    
 }
