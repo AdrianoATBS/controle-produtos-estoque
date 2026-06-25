0a6d38f754de13f7bd00e5833113b1708ed1a5ea
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-06-25 16:17:24 -0300

    refactor(infrastructure): configura a injeção de dependências

---
+++ SistemaControleProdutosEstoque.Infrastructure/DependencyInjection/ConfigureInfrastructure.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using SistemaControleProdutosEstoque.Domain.Interfaces;
+using SistemaControleProdutosEstoque.Infrastructure.Data;
+using SistemaControleProdutosEstoque.Infrastructure.Repositories;
+
+namespace SistemaControleProdutosEstoque.Infrastructure.DependencyInjection;
+
+public static class ConfigureInfrastructure
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+       services.AddDbContext<ApplicationDbContext>(options => 
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddScoped<ICategoriaRepository, CategoriaRepository>();
+        return services;
+    }
+}
