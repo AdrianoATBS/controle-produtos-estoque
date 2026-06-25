cf6425c318681ed291c9c1ce2e1a10518188cee6
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-06-25 14:36:32 -0300

    feat(domain): adiciona contrato para verificar existência de produto por nome

--- SistemaControleProdutosEstoque.Domain/Interfaces/IProdutoRepository.cs
+++ SistemaControleProdutosEstoque.Domain/Interfaces/IProdutoRepository.cs
@@ -7,9 +7,10 @@ public interface IProdutoRepository
     void AdicionarProduto(Produto produto);
     void AtualizarProduto(Produto produto);
     void DeletarProduto(Guid id);
+    bool ExisteProdutoComNome(string nome);
     IEnumerable<Produto> ObterTodosOsProdutos();
     IEnumerable<Produto> ObterProdutosPorCategoria(Guid categoriaId);
-    Produto? ObterProduto(Guid id);
+    Produto? ObterProdutoId(Guid id);
     
     IEnumerable<Produto> ObterProdutosPorNome(string nome);
 
