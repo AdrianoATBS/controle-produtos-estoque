42a4a3d486848b8c60b7e93da8c46d34870a634b
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-07-07 15:17:41 -0300

    feat(categoria): implementa criação de categoria

--- SistemaControleProdutoEstoque.API/Controllers/CategoriasController.cs
+++ SistemaControleProdutoEstoque.API/Controllers/CategoriasController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SistemaControleProdutosEstoque.Application.Requests.Categorias;
+using SistemaControleProdutosEstoque.Application.UseCases;
+using SistemaControleProdutosEstoque.Application.UseCases.Categoria.CriarProdutoUseCase;
 
 namespace SistemaControleProdutoEstoque.API.Controllers
 {
@@ -7,5 +10,19 @@ namespace SistemaControleProdutoEstoque.API.Controllers
     [ApiController]
     public class CategoriasController : ControllerBase
     {
+        private readonly ICriarCategoriaUseCase  _criarCategoriaUseCase;
+        public CategoriasController(ICriarCategoriaUseCase criarCategoriaUseCase)
+        {
+            _criarCategoriaUseCase = criarCategoriaUseCase;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CriarCategoria([FromBody] CriarCategoriaRequest request)
+        {
+            var resultado = await _criarCategoriaUseCase.Executar(request);
+
+            
+            return Created(string.Empty, resultado);
+        }
     }
 }
---
+++ SistemaControleProdutosEstoque.Application/DependecyInjection/ConfigureApplication.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SistemaControleProdutosEstoque.Application.UseCases;
+using SistemaControleProdutosEstoque.Application.UseCases.Categoria.CriarProdutoUseCase;
+using SistemaControleProdutosEstoque.Application.Validators.Categorias;
+
+namespace SistemaControleProdutosEstoque.Application.DependecyInjection;
+
+public static class ConfigureApplication
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddScoped<CriarCategoriaRequestValidator>();
+        services.AddScoped<ICriarCategoriaUseCase, CriarCategoriaUseCase>();
+        return services;
+    }
+}
