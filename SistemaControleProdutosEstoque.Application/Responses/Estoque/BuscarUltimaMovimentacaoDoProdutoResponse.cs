270e75f19226e825b2c9c746deaf1462f5a5ad79
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-08-20 15:12:59 -0300

    refactor(movimentacao-estoque): renomeia propriedade Nome para NomeProduto nos responses

--- SistemaControleProdutosEstoque.Application/Responses/Estoque/BuscarUltimaMovimentacaoDoProdutoResponse.cs
+++ SistemaControleProdutosEstoque.Application/Responses/Estoque/BuscarUltimaMovimentacaoDoProdutoResponse.cs
@@ -6,7 +6,7 @@ public class BuscarUltimaMovimentacaoDoProdutoResponse
 {
     public Guid Id { get; set; }
     public Guid ProdutoId { get; set; }
-    public string Nome { get; set; } = string.Empty;
+    public string NomeProduto { get; set; } = string.Empty;
     public int Quantidade { get; set; }
     public TipoMovimentacao TipoEstoque { get; set; }
     public string DataCriacao { get; set; } = string.Empty;
