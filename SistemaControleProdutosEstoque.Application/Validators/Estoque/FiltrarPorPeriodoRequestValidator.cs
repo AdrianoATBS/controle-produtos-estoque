2ccc4d58142cdaab25c3dda067281303619ab1ec
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-08-25 16:45:31 -0300

    refactor(estoque): move validação de período para validator

---
+++ SistemaControleProdutosEstoque.Application/Validators/Estoque/FiltrarPorPeriodoRequestValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using SistemaControleProdutosEstoque.Application.Requests.Estoque;
+
+namespace SistemaControleProdutosEstoque.Application.Validators.Estoque;
+
+public class FiltrarPorPeriodoRequestValidator : AbstractValidator<FiltrarPorPeriodoRequest>
+{
+    public FiltrarPorPeriodoRequestValidator()
+    {
+        RuleFor(x => x.DataInicio)
+              .NotEmpty()
+              .WithMessage("A data de início é obrigatória.");
+        RuleFor(x => x.DataFim)
+            .NotEmpty()
+            .WithMessage("A data de fim é obrigatória.");
+        RuleFor(x => x)
+            .Must(x => x.DataInicio <= x.DataFim)
+            .WithMessage("A data de inicio não pode ser maior que a data de fim");
+    }
+}
